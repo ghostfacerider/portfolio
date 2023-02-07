f5af9ba34779e3b2fa8d7c65532f57ee3699b05d
Gendy <w0202057@campus.nscc.ca>
2023-02-07 10:13:28 -0400

    changes

--- wp-content/themes/myhellotheme/category.php
+++ wp-content/themes/myhellotheme/category.php
@@ -1,17 +1,34 @@
 <?php
+
 get_header();
+
+$category_name = single_cat_title('', false);
+echo '<h2>'.$category_name. '</h2><br>';
+
 if (have_posts()) :
-    while (have_posts()) : the_post(); ?>
-        <h2><?php the_title();  ?></h2>
-        <?php the_content(); ?>
-<?php
+    while (have_posts()) : the_post();
+    $id = the_ID();
+    $title = get_the_title($id);
+    $excerpt = get_the_excerpt($id);
+    // to get the  hyperlink get_permalink
+    $url_post = get_permalink($id);
+    $url_thumbnail = get_the_post_thumbnail_url($fp->ID, 'medium'); //change the size of the image 
+
+
+    echo '<div class="row"><div class="col-md-3">';  
+    echo '<img src="' . $url_thumbnail . '" class="img-fluid"></div><div class="col-md-9">';
+    echo'<h3><a href="'.$url_post.'" class="img-fluid">'.$title.'</a></h3>';
+    echo $excerpt;
+    echo '</div><?div>';
+        
+
     endwhile;
 else :
     _e('Sorry, no posts matched your criteria.', 'textdomain');
 endif;
 
 // get_sidebar();
-echo "<div> This is the category page </div>";
+
 get_footer();
 
 ?>
--- wp-content/themes/myhellotheme/header.php
+++ wp-content/themes/myhellotheme/header.php
@@ -18,20 +18,22 @@
 <div class="container">
     <header>
         <div class="row">
-            <div class="col-md-6">
-                 <h1><a href="<php echo get_home_url();?>"><?php echo get_bloginfo("name"); ?></a></h1>
-            </div>
+        <div class="col-md-6">
+        <h1><a href="<?php echo get_home_url(); ?>"><?php echo get_bloginfo("name"); ?></a></h1>
+      <?php echo " hello"?>
+        </div>
 
             <div class="col-md-6">
                 <div class="d-flex justify-content-end">
                     <ul class="nav ">
                     <?php
-                        $menu = wp_get_nav_menu_items('Primary Menu');
-                        foreach ($menu as $menu_item) 
-                        {
-                            echo '<li class="nav-item"><a href="' . $menu_item->url . '" class="nav-link">' . $menu_item->title . '</a></li>';
-                        }
-                        ?>
+                    $menu = wp_get_nav_menu_items('Primary Menu');
+
+                    foreach($menu as $menu_item)
+                    {
+                        echo '<li class="nav-item"><a href="'.$menu_item->url.'" class="nav-link">'.$menu_item->title.'</a></li>'; //$menu_item->url." ".$menu_item->title ."<br>";
+                    }
+                    ?>
                     </ul>
                 </div>
              </div>
